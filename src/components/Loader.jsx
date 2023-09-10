6f139c94bbd5b9cb8e2080d224fc835ea4179b11
VikaSukhara <125195456+VikaSukhara@users.noreply.github.com>
2023-09-11 00:21:57 +0200

    loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,12 +1,14 @@
 import { RotatingLines } from 'react-loader-spinner';
 export const Loader = () => {
   return (
-    <RotatingLines
-      strokeColor="grey"
-      strokeWidth="5"
-      animationDuration="0.75"
-      width="96"
-      visible={true}
-    />
+    <div style={{ display: 'flex', justifyContent: 'center', marginTop: '300px' }}>
+      <RotatingLines
+        strokeColor="blue"
+        strokeWidth="5"
+        animationDuration="0.75"
+        width="150"
+        visible={true}
+      />
+    </div>
   );
 };
